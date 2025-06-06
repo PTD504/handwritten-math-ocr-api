dd1024353e97ecfef461cbb2c53ba8bad6bb2a83
PTD504 <22520193@gm.uit.edu.vn>
2025-06-06 22:43:24 +0700

    Add build_vocab.py

---
+++ src/build_vocab.py
@@ -0,0 +1,11 @@
+# file build_vocab.py
+from utils import create_vocab, save_vocab
+from config import config
+
+def main():
+    vocab = create_vocab([config.train_label_path])
+    save_vocab(vocab)
+    print(f"Saved vocab with {len(vocab)} tokens to {config.checkpoint_dir}/vocab.json")
+
+if __name__ == "__main__":
+    main()
